6f4c789b26938c2e94da6e2de46ff5ed401abd65
Gursimran <gursimr2@buffalo.edu>
2020-04-01 16:10:49 -0400

    SR Outline

--- gursimr2/src/sr.cpp
+++ gursimr2/src/sr.cpp
@@ -1,5 +1,6 @@
 #include "../include/simulator.h"
-
+#include <bits/stdc++.h> 
+using namespace std;
 /* ******************************************************************
  ALTERNATING BIT AND GO-BACK-N NETWORK EMULATOR: VERSION 1.1  J.F.Kurose
 
@@ -15,29 +16,81 @@
 
 /********* STUDENTS WRITE THE NEXT SEVEN ROUTINES *********/
 
+int base,nextseqnum,winsize,maxseqnum,bufferseqnum,expectedseqnum;
+map<int, string> buffer;
+pkt sendpkt,bufferpkt,ackpkt; 
+void makepacket(pkt &p,int n){
+p.checksum=0;
+for(int i=0;i<20;i++){
+  p.payload[i]=buffer[n][i];
+  p.checksum+=(int)p.payload[i];
+}
+p.seqnum = n;
+p.acknum = n;
+p.checksum += p.seqnum + p.acknum;
+}
+void sendpacket(pkt &p){
+tolayer3(0,p);
+}
 /* called from layer 5, passed the data to be sent to other side */
 void A_output(struct msg message)
 {
+  buffer[bufferseqnum]=message.data;
+  bufferseqnum++;
+   if((nextseqnum) < (base + winsize) ){
+    //makepacket
+    makepacket(sendpkt,nextseqnum);
+    //sendpacket
+    sendpacket(sendpkt);
+    if(base==nextseqnum)//change timer thing
+      starttimer(0,30);
+    nextseqnum++;
+  }
+  else{
+    //Refuse 
+  }
 
 }
 
 /* called from layer 3, when a packet arrives for layer 4 */
 void A_input(struct pkt packet)
 {
-
+//major change
+int checksum = 0;
+checksum += packet.seqnum + packet.acknum;
+if(checksum == packet.checksum){
+      base = packet.acknum + 1;
+      if(base==nextseqnum)
+        stoptimer(0);
+      else
+      {
+        stoptimer(0);
+        starttimer(0,30);
+      }
+}
+else{
+  //do nothing
+}
 }
 
 /* called when A's timer goes off */
 void A_timerinterrupt()
 {
-
+  //change this
+starttimer(0,30);
+for(int i=base;i<=nextseqnum-1;i++){
+  makepacket(bufferpkt,i);
+  sendpacket(bufferpkt);}
 }  
 
 /* the following routine will be called once (only) before any other */
 /* entity A routines are called. You can use it to do any initialization */
 void A_init()
 {
-
+    base=1;
+    nextseqnum=1;
+    winsize=getwinsize();
+    bufferseqnum=1;
 }
 
 /* Note that with simplex transfer from a-to-B, there is no B_output() */
@@ -45,6 +98,23 @@ void A_init()
 /* called from layer 3, when a packet arrives for layer 4 at B*/
 void B_input(struct pkt packet)
 {
+  //major change
+   int checksum = 0;
+for(int i=0;i<20;i++){
+  checksum += packet.payload[i];
+}
+checksum += packet.seqnum + packet.acknum;
+if(checksum == packet.checksum && packet.seqnum==expectedseqnum){ // If not corrupted and has expected sequence number
+tolayer5(1,packet.payload);
+ackpkt.seqnum=expectedseqnum;
+ackpkt.acknum=expectedseqnum;
+ackpkt.checksum=ackpkt.seqnum+ackpkt.acknum;
+tolayer3(1,ackpkt);
+expectedseqnum++;
+}
+else{ // Default
+tolayer3(1,ackpkt);
+}
 
 }
 
@@ -52,5 +122,7 @@ void B_input(struct pkt packet)
 /* entity B routines are called. You can use it to do any initialization */
 void B_init()
 {
-
+expectedseqnum=1;
+ackpkt.seqnum=0;
+ackpkt.acknum=0;
 }
